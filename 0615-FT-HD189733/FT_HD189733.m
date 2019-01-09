e19928da4cd6dbae4ecf6084cdf4c8b8b3b3b3d4
jinglinzhao <jinglinzhao@gmail.com>
2019-01-09 14:48:27 +1100

    Update FT_HD189733.m

--- 0615-FT-HD189733/FT_HD189733.m
+++ 0615-FT-HD189733/FT_HD189733.m
@@ -52,9 +52,9 @@ for n = 1:N_FILE
     A           = importdata(dat_name);
 
 %     A_spline    = A_spline .* window;
-    if mod(n,5) == 1
+%     if mod(n,5) == 1
         plot(x, A-A1, 'k-')
-    end
+%     end
 %     plot(x, A-A1, 'k')
     [FFT_frequency, FFT_power(:, n), Y(:, n)] = FUNCTION_FFT(A, Fs);
     
@@ -71,7 +71,7 @@ saveas(gcf,'1-Differential_line_Profile','png')
 close(h)
 
 % Determine the midpoint the equally divides the power spectrum %
-cutoff_power= max(max(FFT_power)) * 0.0001;
+cutoff_power= max(max(FFT_power)) * 0.001;
 f_max       = max(FFT_frequency(FFT_power(:,1) > cutoff_power));
 n           = abs(FFT_frequency) <= f_max;
 power_sum   = sum(FFT_power(n,1));
@@ -112,7 +112,7 @@ close(h)
 % Phase angle %
 %%%%%%%%%%%%%%%
 h = figure;
-plot(FFT_frequency, unwrap(angle(Y(:, 51))), '.')
+plot(FFT_frequency, unwrap(angle(Y(:, 1))), '.')
 title('Phase angle (Rotation phase = 0.51)')
 xlabel('FT frequency (1 / velocity in wavelength)')
 ylabel('Phase angle [radian]')
@@ -124,32 +124,6 @@ close(h)
 %%%%%%%%%%%%%%%%%%%%%
 % Phase angle -> RV %
 %%%%%%%%%%%%%%%%%%%%%
-if 0    % old version
-    slope = zeros(1,N_FILE);
-    RV_FT = zeros(1,N_FILE);
-    h = figure; 
-        hold on
-        for i = 1:N_FILE
-            n = (size(FFT_frequency,2)/2+1-15):(size(FFT_frequency,2)/2+1+15);
-            xx = FFT_frequency(n);
-            yy = angle(Y(n, i)) - angle(Y(n, 1));
-            plot(xx, yy, '-')
-            title('Phase angle (relative to 1st epoch)')
-            xlabel('FT frequency (1 / velocity in wavelength)')
-            ylabel('Phase angle [radian]')
-
-            % Phase angle -> RV
-            p = polyfit(xx, unwrap(angle(Y(n, i)))',3);
-            slope(i) = p(3);
-            RV_FT1 = -slope(1) / (2*pi);
-            RV_FT(i) = -slope(i) / (2*pi) - RV_FT1;
-        end
-        hold off
-        saveas(gcf,'4-Relative_phase_angle','png')
-    close(h)
-end
-
-
 n       = abs(FFT_frequency) <= f_max;
 slope   = zeros(1,N_FILE);
 RV_FT   = zeros(1,N_FILE);
@@ -159,9 +133,9 @@ hold on
 for i = 1:N_FILE
     xx  = FFT_frequency(n);
     yy  = angle(Y(n, i)) - angle(Y(n, 1));
-    if mod(i,5) == 1
+%     if mod(i,5) == 1
         plot(xx, yy, 'k-')
-    end
+%     end
     % Phase angle -> RV
     weight = FFT_power(n,i)';
     [fitresult, gof] = createFit(xx, yy', weight);
@@ -186,9 +160,9 @@ hold on
 for i = 1:N_FILE
     xx  = FFT_frequency(nl);
     yy  = angle(Y(nl, i)) - angle(Y(nl, 1));
-    if mod(i,5) == 1
+%     if mod(i,5) == 1
         plot(xx, yy, 'k-')
-    end
+%     end
     % Phase angle -> RV
     weight = FFT_power(nl,i)';
     [fitresult, gof] = createFit(xx, yy', weight);
@@ -214,9 +188,9 @@ hold on
 for i = 1:N_FILE
     xx  = FFT_frequency(n);
     yy  = angle(Y(n, i)) - angle(Y(n, 1));
-    if mod(i,5) == 1
+%     if mod(i,5) == 1
         plot(xx, yy, 'k-')
-    end
+%     end
     % Phase angle -> RV
     weight = FFT_power(n,i)';
     [fitresult, gof] = createFit(xx, yy', weight);
@@ -237,12 +211,30 @@ XX = (RV_FT-mean(RV_FT))'*1000;
 YY = (RV_FTL-mean(RV_FTL))'*1000;
 ZZ = (RV_FTH-mean(RV_FTH))'*1000;
 
+idx_t   = (MJD>54340.98) & (MJD<54341.11);
+GGG = RV_HARPS(idx_t);
+XXX = XX(idx_t);
+YYY = YY(idx_t);
+ZZZ = ZZ(idx_t);
+BI = b(1) + b(2) * MJD2;
+
+dlmwrite('GG.txt', GGG)
+dlmwrite('XX.txt', XXX)
+dlmwrite('YY.txt', YYY)
+dlmwrite('ZZ.txt', ZZZ)
+dlmwrite('BI.txt', BI)
+dlmwrite('XY.txt', jitter_smooth2)
+
+
+ 
+
+
 
 % Time sequence %
 h = figure; 
     hold on
-    errorbar(MJD, RV_HARPS, RV_noise , 'r.', 'MarkerSize', 20)
-    errorbar(MJD, XX, RV_noise , 'b.', 'MarkerSize', 20)
+    errorbar(MJD-min(MJD), RV_HARPS, RV_noise , 'r.', 'MarkerSize', 20)
+    errorbar(MJD-min(MJD), XX, RV_noise , 'b.', 'MarkerSize', 20)
     grid on
     hold off
     xlabel('Time [d]')
@@ -268,24 +260,148 @@ RV_FT = YY;
 % jitter_proto = (ZZ - RV_HARPS) + (RV_HARPS - YY); 
 % jitter_proto = (ZZ - RV_HARPS);
 jitter_proto = (RV_HARPS - YY);
+
 t_smooth1 = linspace(54301.08, 54301.24, 1000);
 y_smooth1 = FUNCTION_GAUSSIAN_SMOOTHING(MJD, jitter_proto, 1./RV_noise.^2, t_smooth1, width);
 t_smooth2 = linspace(54340.98, 54341.14, 1000);
 y_smooth2 = FUNCTION_GAUSSIAN_SMOOTHING(MJD, jitter_proto, 1./RV_noise.^2, t_smooth2, width);
 
+t_smooth  = linspace(53986, 53986.18, 1000);
+y_smooth = FUNCTION_GAUSSIAN_SMOOTHING(MJD, jitter_proto, 1./RV_noise.^2, t_smooth, width);
+
+
 h = figure; 
     hold on
-    plot(t_smooth1, y_smooth1)
-    plot(t_smooth2, y_smooth2)
-    errorbar(MJD, jitter_proto, RV_noise, '.', 'MarkerSize', 20)
+%     plot(t_smooth1, y_smooth1)
+%     plot(t_smooth2, y_smooth2)
+%     plot(t_smooth, y_smooth)
+    errorbar(MJD - min(MJD), jitter_proto, RV_noise, '.', 'MarkerSize', 20)
     hold off
     title('Jittere model')
     xlabel('BJD')    
     ylabel('Jittere model [m/s]')
     grid on
 close(h)
+
+
+
+
+%%%%%%%%%%%%%%%%%%
+% Fitting Part 0 %   
+%%%%%%%%%%%%%%%%%%
+weight = 1./RV_noise.^2;
+RV_HARPS   = RV_HARPS - mean(RV_HARPS);
+jitter     = jitter_proto;
+
+% Linear part %
+MJD    = MJD - min(MJD);
+idx_linear  = (MJD < 0.024) | (MJD > 0.1);
+MJD_linear = MJD(idx_linear);
+RV_HARPS_linear    = RV_HARPS(idx_linear);
+weight_linear       = weight(idx_linear);
+fun_b = @(b) sum((b(1) + b(2) * MJD_linear - RV_HARPS_linear).^2 .* weight_linear);
+b0 = [max(RV_HARPS), -max(RV_HARPS)/max(MJD)];
+options = optimoptions(@fminunc,'Algorithm','quasi-newton');
+b = fminunc(fun_b,b0,options);
+           
+            
+h = figure;
+    subplot(16,1,1:14)
+    hold on 
+    scatter(MJD*24, RV_FT, 30, 'rD', 'MarkerFaceColor', 'r', 'MarkerFaceAlpha', 0.5);
+    scatter(MJD*24, RV_HARPS, 30, 'bo', 'MarkerFaceColor', 'b', 'MarkerFaceAlpha', 0.5);
+%     errorbar(MJD2, RV_FT2, RV_noise2, 'k.', 'MarkerSize', 0.1);
+%     errorbar(MJD2, RV_HARPS2, RV_noise2, 'b.', 'MarkerSize', 0.1);
+    plot1 = plot(MJD*24, b(1) + b(2) * MJD, 'b--', 'LineWidth', 2);
+    plot1.Color(4) = 0.4;        
     
-% Fitting Part 1 %    
+    hold off
+    xlim([-0.003*24 (max(MJD)+0.003)*24])
+    legend('RV_{FT,L}', 'RV_{HARPS}', 'Linear trend')
+%     title('Transit RV curve')
+    ylabel('RV [m/s]')
+    ylim([-59 49])
+    set(gca,'fontsize', 15)
+    set(gca,'xticklabel',[])    
+    grid on
+
+    % Jitter part %
+    subplot(16,1,15:16)
+    t2_plot         = linspace(min(MJD), max(MJD), 1001);
+    jitter2_plot    = FUNCTION_GAUSSIAN_SMOOTHING(MJD, jitter, weight, t2_plot, width);
+    hold on
+    scatter(MJD*24, jitter - mean(jitter), 30, 'ks', 'MarkerFaceColor', 'k', 'MarkerFaceAlpha', 0.5);
+    errorbar(MJD*24, jitter - mean(jitter), RV_noise, 'k.', 'MarkerSize', 0.1)
+    plot2 = plot(t2_plot*24, jitter2_plot - mean(jitter), 'k-', 'LineWidth',2);
+    plot2.Color(4) = 0.4;       
+    hold off
+    xlim([-0.003*24 (max(MJD)+0.003)*24])
+    ylim([-3.2 3.2])
+    xlabel('Time [h]')
+    ylabel('\DeltaRV_{L} [m/s]')
+    set(gca,'fontsize', 15)
+    saveas(gcf,'8-Proto_jitter0','png')
+close(h)
+
+jitter_smooth  = FUNCTION_GAUSSIAN_SMOOTHING(MJD, jitter, weight, MJD, width);
+RV_RM = RV_HARPS - (b(1) + b(2) * MJD);
+fun_a = @(a) sum((a(1) * jitter_smooth + a(2) - RV_RM).^2 .* weight);
+options = optimoptions(@fminunc,'Algorithm','quasi-newton');
+a0 = [10, 0];
+a = fminunc(fun_a,a0,options);
+
+h = figure;
+subplot(16,1,1:14)       % add first plot in 2 x 1 grid
+    hold on 
+    scatter(MJD*24, a(1) * jitter + a(2), 30, 'ks', 'MarkerFaceColor', 'k', 'MarkerFaceAlpha', 0.5);
+    plot3 = plot(t2_plot*24, a(1) * jitter2_plot + a(2) , 'k', 'LineWidth', 2);
+    plot3.Color(4) = 0.4;  
+    scatter(MJD*24, RV_RM, 30, 'bo', 'MarkerFaceColor', 'b', 'MarkerFaceAlpha', 0.5);    
+%     jitter_final_Z = a(1) * jitter2 + a(2);
+%     err_jitter_final_Z = RV_noise2 * sqrt(3) * a(1);
+    errorbar(MJD*24, a(1) * jitter + a(2), (RV_noise * a(1)), 'k.', 'MarkerSize', 0.1)
+    errorbar(MJD*24, RV_RM, RV_noise, 'b.', 'MarkerSize', 0.1)
+    xlim([-0.003*24 (max(MJD)+0.003)*24])
+    ylim([-85 85])
+    ylabel('RV [m/s]')
+    set(gca,'fontsize', 15)
+    set(gca,'xticklabel',[])
+    grid on 
+    legend('\alpha \DeltaRV_{L}', 'WMA', 'RM effect')
+    hold off
+    
+subplot(16,1,15:16)       % add first plot in 2 x 1 grid
+    hold on
+%     scatter(MJD2, a(1) * jitter2 + a(2) - RV_RM2, 30, 'ks', 'MarkerFaceColor', 'k', 'MarkerFaceAlpha', 0.5)
+%     errorbar(MJD2, a(1) * jitter2 + a(2) - RV_RM2, (RV_noise2 * sqrt(2) * a(1)), 'k.', 'MarkerSize', 0.1)
+    scatter(MJD*24, a(1) * jitter_smooth + a(2) - RV_RM, 30, 'ko', 'MarkerFaceColor', 'k', 'MarkerFaceAlpha', 0.5)
+%     plot5 = plot(MJD2, a(1) * jitter_smooth2 + a(2) - RV_RM2, 'k', 'LineWidth', 2);
+%     plot5.Color(4) = 0.4;
+%     plot4 = plot(MJD2, a(1) * jitter_smooth2 + a(2) - RV_RM2 , 'k', 'LineWidth', 2);
+%     plot4.Color(4) = 0.4;
+    hold off
+    xlabel('Time [h]')
+    ylabel('Residual [m/s]')
+%     ylim([-30 30])
+    xlim([-0.003*24 (max(MJD)+0.003)*24])
+    ylim([-12 12])
+    set(gca,'fontsize', 15)
+    saveas(gcf,'9-RM_fit0','png')
+close(h)
+
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%
+% Fitting Part 1 %   
+%%%%%%%%%%%%%%%%%%
 idx_t   = (MJD>54301.08) & (MJD<54301.241);
 MJD1    = MJD(idx_t);    
 weight1 = 1./RV_noise(idx_t).^2;
